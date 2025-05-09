2e99df2fbb7228d8d22fec776b26ed73702ab9ab
Max <max4real69@gmail.com>
2025-05-09 20:28:34 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "Ranma Saotome",
   botOwner: 'Max', // Your name
   ownerNumber: '923298335681', '+358 46 6105300' , //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
