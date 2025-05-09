bc754d2f5a15c07d3dd89a9aee270955d94e57c6
Max <max4real69@gmail.com>
2025-05-09 20:26:08 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'Max', // Your name
-  ownerNumber: '923298335681', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '923298335681', '+358 46 6105300' , //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
