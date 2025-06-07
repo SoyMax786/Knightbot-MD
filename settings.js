d6c059c8fbc571c7407c2f879789763609372b43
Max <max4real69@gmail.com>
2025-06-07 16:16:46 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Ranma Saotome",
   botOwner: 'Max', // Your name
-  ownerNumber: ['923298335681', '358466105300'], // Set your numbers here without + and without space
+  ownerNumber: ['923260018057', '923298335681'], // Set your numbers here without + and without space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
